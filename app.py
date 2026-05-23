fa9156921a48e5619804e70f08be1bfa2d31c5a2
VittorioD <vittorio.dinata@gmail.com>
2026-05-23 07:56:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ def page_statistics():
     # ── ② Action Distribution ─────────────────────────────────────────────────
     st.subheader("② Action Distribution in Training Data")
 
-    action_counts = {"Fold": 656484, "Call": 230070, "All-In":53800 , "Raise": 59646}
+    action_counts = {"Fold": 656484, "Call": 230070, "Raise": 59646, "All-In":53800}
     fig1, ax1 = plt.subplots(figsize=(8, 4))
     bars = ax1.bar(
         action_counts.keys(), action_counts.values(),
